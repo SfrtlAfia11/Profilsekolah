7f2e6628b6b98427a26acebd3c276b7b0d45f89a
SfrtlAfia11 <afiasofirotul@gmail.com>
2021-02-04 08:41:17 +0700

    tambahan fitur profil

--- app/src/main/java/com/example/profilsekolah/MainActivity.java
+++ app/src/main/java/com/example/profilsekolah/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.profilsekolah;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+    }
+    public void profil(View view){
+        Intent intent=new Intent(MainActivity.this, ProfilActivity.class);
+        startActivity(intent);
     }
 }
